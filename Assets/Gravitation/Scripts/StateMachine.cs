dcd18be56b73e8ae28aad3078c1e1f7641c6bef2
Sashakt <sashakttripathi@users.noreply.github.com>
2020-10-19 22:59:44 +0530

    lite

--- Assets/Gravitation/Scripts/StateMachine.cs
+++ Assets/Gravitation/Scripts/StateMachine.cs
@@ -5,7 +5,26 @@ using UnityEngine;
 public class StateMachine : MonoBehaviour
 {
     // This is a general script to run all activities which are basically state machines
+    public Animator _activityController;
+    public GameObject gripMeter;
+    private int stateTracker;
 
+    // private void Update()
+    // {
+    //     switch (stateTracker)
+    //     {
+    //         case 1:
+    //             break;
+
+    //         case 2:
+    //             break;
+
+    //         case 3:
+    //             break;
+
+    //         default:
+    //     }
+    // }
 
 
 }
