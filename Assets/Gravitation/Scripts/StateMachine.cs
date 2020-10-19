b7888fe45f41e99321568a60d9b961cebe11431e
Sashakt <sashakttripathi@users.noreply.github.com>
2020-10-19 15:59:42 +0530

    lite

---
+++ Assets/Gravitation/Scripts/StateMachine.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+    // This is a general script to run all activities which are basically state machines
+
+
+
+}
